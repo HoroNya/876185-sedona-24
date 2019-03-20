7cc723de91ecfcfbfd507e558018d447bb0b60c4
HoroNya <46902298+HoroNya@users.noreply.github.com>
2019-03-20 21:49:26 +0300

    fix

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+  var link = document.querySelector(".search-button");
+
+  var popup = document.querySelector(".form-search-none");
+
+  link.addEventListener("click", function (evt) {
+
+    popup.classList.add("form-search");
+});
